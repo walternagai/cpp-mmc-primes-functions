cee6c70dea8d6a5e9261324ccb7798ba0273bae8
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2021-06-15 10:28:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,94 +2,66 @@
 using namespace std;
 
 /*
-  mostraDiaDaSemana (k)
-  k: número da semana
-  Função para mostrar o dia da semana a partir de k
+  MDC (a, b)
+  Máximo Divisor Comum entre dois números
 */
-string mostraDiaDaSemana (int k) {
-  string dia;
-  switch (k) {
-    case 0: 
-      dia = "Sabado";
-      break;
-    case 1: 
-      dia = "Domingo";
-      break;
-    case 2: 
-      dia = "Segunda-feira";
-      break;
-    case 3: 
-      dia = "Terca-feira";
-      break;
-    case 4: 
-      dia = "Quarta-feira";
-      break;
-    case 5: 
-      dia = "Quinta-feira";
-      break;
-  default:
-    dia = "Sexta-feira";
-  }
-  return dia;
+int MDC (int a, int b) {
+  /*
+  Algoritmo de Euclides
+
+  enquanto b diferente de zero faça
+    r recebe resto da divisão entre a e b
+    a recebe o valor de b
+    b recebe o valor de r
+  fim-enquanto
+  retorne a
+  */
+  return 0;
 }
 
 /*
-  ehAnoBissexto (ano)
-  ano: ano de quatro dígitos
-  Função para determinar se o ano é bissexto ou não
+  MMC (a, b)
+  Mínimo Múltiplo Comum entre dois números
 */
-bool ehAnoBissexto (int ano) {
-  if ((ano % 400 == 0) || 
-      ((ano % 4 == 0) && (ano % 100 != 0)))
-    return true;
+int MMC (int a, int b) {
+  /*
+    MMC(a, b) * MDC(a, b) = a * b
+
+    Para calcular o MMC, pode-se usar uma das fórmulas:
+    a) MMC(a, b) = a * (b / MDC(a, b)) OU
+    b) MMC(a, b) = b * (a / MDC(a, b))
+  */
+  return 0;
+}
+
+/*
+  saoPrimos (a, b)
+  Dois números são primos se o MDC é igual a 1
+*/
+bool saoPrimos (int a, int b) {
+  /*
+    se o MDC (a, b) é igual a 1 retorne true
+    caso contrário retorne falso
+  */    
   return false;
 }
 
-/*
-  validarData (dia, mes, ano)
-  dia: dia de um mes
-  mes: mes de um ano
-  ano: ano de quatro digitos
-  Função para validar data de um calendário.
-*/
-bool validarData (int dia, int mes, int ano) {
-  if (ano < 0)
-    return false;
-  else if (ano > 2021)
-    return false;
-  else if (mes <= 0 || mes > 12)
-    return false;
-  else if (mes == 1 || mes == 3 || mes == 5 ||
-           mes == 7 || mes == 8 || mes == 10 ||
-           mes == 12) {
-          if (dia <= 0 || dia > 31)
-            return false;
-  } else if (mes == 4 || mes == 6 || 
-            mes == 9 || mes == 11) {
-          if (dia <= 0 || dia > 30)
-            return false;
-  } else if (mes == 2) {
-      if (dia >= 29 && !ehAnoBissexto(ano))
-        return false;
-  }
-  return true;
-}
-
 int main() {
-  int k, dia, mes, ano; 
-  bool dataValida = true;
+  int numA, numB;
 
-  cin >> dia >> mes >> ano;
-  dataValida = validarData (dia, mes, ano);
+  cin >> numA >> numB;
 
-  // REESCREVER ENQUANTO (davaValida) EXECUTE as instruções
-  if (dataValida) {
-    k = dia + 2 * mes + (3*(mes+1))/5 + ano + ano/4 - ano/100 + ano/400 + 2;
-    k = k % 7;
-    cout << mostraDiaDaSemana(k) << endl;
-    // ADICIONAR ler as variáveis dia, mes e ano
-    // ADICIONAR chamar validarData para as variáveis dia, mês e ano
-  }
+  cout << "MDC(" << numA << ", " << numB << ") = "
+       << MDC (numA, numB) << endl;
+  cout << "MMC(" << numA << ", " << numB << ") = "
+       << MMC (numA, numB) << endl;
+  
+  cout << numA << " e " << numB;
+  bool primos = saoPrimos(numA, numB);
+  if (primos == true)
+    cout << " sao primos.\n";
+  else
+    cout << " não sao primos.\n";
 
   return 0;
 }
